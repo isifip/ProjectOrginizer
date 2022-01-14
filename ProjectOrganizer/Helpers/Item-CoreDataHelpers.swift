e113e825db3020bbae624a3ddddd834a83081014
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-14 23:11:14 +0400

    - Add helpers for both item and project entities - create properties to avoid nil coalescing in our code - both items and projects have examples for previews - implement sorting for items, high priority comes first and etc.. - completion amount for each project

---
+++ ProjectOrganizer/Helpers/Item-CoreDataHelpers.swift
@@ -0,0 +1,33 @@
+//
+//  Item-CoreDataHelpers.swift
+//  ProjectOrganizer
+//
+//  Created by Irakli Sokhaneishvili on 14.01.22.
+//
+
+import Foundation
+
+extension Item {
+    // this helps us to avoid optionals in our code
+    var itemTitle: String {
+        title ?? ""
+    }
+    var itemDetail: String {
+        detail ?? ""
+    }
+    var itemCreationData: Date {
+        creationDate ?? Date()
+    }
+    
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let item = Item(context: viewContext)
+            item.title = "Example Item"
+            item.detail = "This is an example item"
+            item.priority = 3
+            item.creationDate = Date()
+            return item
+    }
+}
