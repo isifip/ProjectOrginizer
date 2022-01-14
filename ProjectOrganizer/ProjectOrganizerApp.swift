3fa19d1a20c6138754286c3863afe9486e1969e7
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-14 19:47:02 +0400

    Initial commit.

---
+++ ProjectOrganizer/ProjectOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOrganizerApp.swift
+//  ProjectOrganizer
+//
+//  Created by Irakli Sokhaneishvili on 14.01.22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
