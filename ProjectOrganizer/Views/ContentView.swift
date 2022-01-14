7f940d866f62c4d5d2abf260492ee84d9b21b664
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-14 22:38:46 +0400

    - Present HomeView and ProjectsViews in ContentView as a TabView. - Closed and Open projects are different tab views

--- ProjectOrganizer/Views/ContentView.swift
+++ ProjectOrganizer/Views/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open")
+                }
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Image(systemName: "checkmark")
+                    Text("Closed")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
     }
 }
