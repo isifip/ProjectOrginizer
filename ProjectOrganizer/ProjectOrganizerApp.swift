3d4f3f28b44cfd3ac49486086d1bddcbaa5f46f6
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-15 20:36:12 +0400

    - Update values when change happens instantly with binding OnChange. - Save updates.

--- ProjectOrganizer/ProjectOrganizerApp.swift
+++ ProjectOrganizer/ProjectOrganizerApp.swift
@@ -22,6 +22,13 @@ struct ProjectOrganizerApp: App {
             ContentView()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
+                .onReceive(NotificationCenter.default.publisher(
+                                for: UIApplication.willResignActiveNotification),
+                                perform: save
+                )
         }
+    }
+    func save(_ note: Notification) {
+        dataController.save()
     }
 }
