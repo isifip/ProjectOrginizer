60658194ff6bfa96272c74e6be0c2ae4b492688e
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-17 18:11:55 +0400

    - Add HomeView - ScrollView, which inside contains horizontal scroll view representing projects overview

---
+++ ProjectOrganizer/Extensions/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  ProjectOrganizer
+//
+//  Created by Irakli Sokhaneishvili on 17.01.22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
--- ProjectOrganizer/Views/HomeView.swift
+++ ProjectOrganizer/Views/HomeView.swift
@@ -5,6 +5,7 @@
 //  Created by Irakli Sokhaneishvili on 14.01.22.
 //
 
+import CoreData
 import SwiftUI
 
 struct HomeView: View {
@@ -14,15 +15,56 @@ struct HomeView: View {
     
     @EnvironmentObject var dataController: DataController
     
+    @FetchRequest(entity: Project.entity(),
+                  sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)],
+                  predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
+    
+    let items: FetchRequest<Item>
+    
+    var projectRows: [GridItem] {
+        [GridItem(.fixed(100))]
+    }
+    
+    init() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "completed = false")
+        request.sortDescriptors = [
+            NSSortDescriptor(keyPath: \Item.priority, ascending: false)
+        ]
+        request.fetchLimit = 10
+        request.fetchOffset = 3
+        items = FetchRequest(fetchRequest: request)
+    }
+    
     var body: some View {
         NavigationView {
-            VStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: projectRows) {
+                            ForEach(projects) { project in
+                                VStack(alignment: .leading) {
+                                    Text("\(project.projectItems.count)")
+                                        .font(.caption)
+                                        .foregroundColor(.secondary)
+                                    Text("\(project.projectTitle)")
+                                        .font(.title2).fontWeight(.semibold)
+                                    ProgressView(value: project.completionAmount)
+                                        .tint(Color(project.projectColor))
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .background(Color.systemGroupedBackground.ignoresSafeArea())
+            .navigationTitle("Home")
+            .toolbar {
                 Button("Add Data") {
                     dataController.deleteAll()
                     try? dataController.createSampleData()
                 }
             }
-            .navigationTitle("Home")
         }
     }
 }
